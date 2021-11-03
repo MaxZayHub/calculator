3171335ae7db43fc02ce381832c42d545be0c83b
MaxZay <maximzay2017@gmail.com>
2021-11-03 20:10:05 +0300

    fix: change the rendering of buttons

--- src/index.js
+++ src/index.js
@@ -49,66 +49,225 @@ let arr = [
     text: "%",
   },
   {
-    type: "advancedArifmetic",
+    type: "basicArifmetic",
     value: "/",
     text: "\u00F7",
   },
   {
     type: "advancedArifmetic",
     value: "2^nd",
+    text: `2${"nd".sup().small()}`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "^2",
+    text: `x${"2".sup().small()}`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "^3",
+    text: `x${"3".sup().small()} `,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "^",
+    text: `x${"y".sup().small()}`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "e^",
+    text: `e${"x".sup().small()}`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "10^",
+    text: `10${"x".sup().small()}`,
+  },
+  {
+    type: "number",
+    value: "7",
+    text: "7",
+  },
+  {
+    type: "number",
+    value: "8",
+    text: "8",
+  },
+  {
+    type: "number",
+    value: "9",
+    text: "9",
+  },
+  {
+    type: "basicArifmetic",
+    value: "*",
+    text: "\u00D7",
+  },
+  {
+    type: "advancedArifmetic",
+    value: "1/",
+    text: `1/x`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "sqrt",
+    text: "\u221Ax",
+  },
+  {
+    type: "advancedArifmetic",
+    value: "sqrt3",
+    text: `\u221Bx`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "xy",
+    text: `${"y".sup().small()}\u221Ax`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "ln",
+    text: `ln`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "log10",
+    text: `log${"10".sub().small()}`,
+  },
+  {
+    type: "number",
+    value: "4",
+    text: "4",
+  },
+  {
+    type: "number",
+    value: "5",
+    text: "5",
+  },
+  {
+    type: "number",
+    value: "6",
+    text: "6",
+  },
+  {
+    type: "basicArifmetic",
+    value: "-",
+    text: "\u2212",
+  },
+  {
+    type: "advancedArifmetic",
+    value: "!",
+    text: "x!",
+  },
+  {
+    type: "advancedArifmetic",
+    value: "sin",
+    text: "sin",
+  },
+  {
+    type: "advancedArifmetic",
+    value: "cos",
+    text: "cos",
+  },
+  {
+    type: "advancedArifmetic",
+    value: "tan",
+    text: `tan`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "e",
+    text: `e`,
+  },
+  {
+    type: "advancedArifmetic",
+    value: "EE",
+    text: `EE`,
+  },
+  {
+    type: "number",
+    value: "1",
+    text: "1",
+  },
+  {
+    type: "number",
+    value: "2",
     text: "2",
   },
   {
-    type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    type: "number",
+    value: "3",
+    text: "3",
+  },
+  {
+    type: "basicArifmetic",
+    value: "+",
+    text: "\u002B",
+  },
+  {
+    type: "leftCorner",
+    value: "Rad",
+    text: `Rad`,
   },
   {
     type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    value: "sinh",
+    text: `sinh`,
   },
   {
     type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    value: "cosh",
+    text: "cosh",
   },
   {
     type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    value: "tanh",
+    text: `tanh`,
   },
   {
     type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    value: "pi",
+    text: `\u03C0`,
   },
   {
     type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    value: "Rand",
+    text: `Rand`,
   },
   {
-    type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    type: "zero",
+    value: "0",
+    text: "0",
   },
   {
-    type: "advancedArifmetic",
-    value: "(",
-    text: "(",
+    type: "number",
+    value: ",",
+    text: ",",
+  },
+  {
+    type: "rightCorner",
+    value: "=",
+    text: "=",
   },
 ]
 
-let lightGrayButtons = document.querySelectorAll(".light-gray")
-let grayButtons = document.querySelectorAll("gray")
+const buttonBlock = document.querySelector(".buttons-block")
 
-// lightGrayButtons.forEach((element) => {
-//   element.addEventListener("click", (event) => {
-//     console.log(event);
-//     if (input.value === "0" && event.target.innerHTML !== ",") {
-//       input.value = "";
-//     }
-//     input.value += event.target.innerHTML;
-//   });
-// });
+arr.forEach((item) => {
+  let button = document.createElement("button")
+  if (item.type === "advancedArifmetic") {
+    button.className = "button gray"
+  } else if (item.type === "basicArifmetic") {
+    button.className = "button orange"
+  } else if (item.type === "number") {
+    button.className = "button light-gray"
+  } else if (item.type === "zero") {
+    button.className = "button light-gray zero"
+  } else if (item.type === "rightCorner") {
+    button.className = "button orange border-right"
+  } else if (item.type === "leftCorner") {
+    button.className = "button gray border-left"
+  }
+  button.dataset.value = item.value
+  button.innerHTML = item.text
+  buttonBlock.appendChild(button)
+})
